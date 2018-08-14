8ebf6046e087447589202f2fb9c7cac52f34284d
Shubhank Saxena <saxena.shubhank.19@gmail.com>
2018-08-15 01:38:46 +0530

    first commit

---
+++ github.js
@@ -0,0 +1,23 @@
+class Github{
+	constructor(){
+		this.client_id= '66dda663b4439c5118fd';
+		this.client_secret= '99dcc812c05c3a7d2d5e560cfd9faf8c9c6bdf89';
+		this.repos_count= 5;
+		this.repos_sort='created: asc'; 
+	}
+
+	async getUser(user){
+		const profileResponse= await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+		const repoResponse= await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+		const profile= await profileResponse.json();
+
+		const repos= await repoResponse.json();
+
+		return{
+			profile,
+			repos
+		}		
+	}
+}
